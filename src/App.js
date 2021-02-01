298630d5463e29cc9a9120a5c04ca93588a67c4b
Виталий Рощупкин <vroshchupkin@Magnum.local>
2021-02-01 17:12:28 +0300

    create MainPages and change App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import MainPages from './MainPages';
 
 const App = () => {
-  return <Main />;
-};
-
-const Main = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Greeting my dear friend!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <MainPages />;
 };
 
 export default App;
---
+++ src/MainPages.js
@@ -0,0 +1,23 @@
+import './App.css';
+import logo from './logo.svg';
+
+const MainPages = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Greeting my dear friend!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default MainPages;
