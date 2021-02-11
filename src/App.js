4766cfb96de34d9d06a72be65fc163fa3256ca49
Виталий Рощупкин <vroshchupkin@Magnum.local>
2021-02-11 16:49:00 +0300

    create Messages.jsx

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
+import Messages from './components/Messages/Messages';
 
 const App = () => {
   return (
     <div className="wrapper">
       <Header />
-      <Profile />
       <Navbar />
+      <Messages />
     </div>
   );
 };
