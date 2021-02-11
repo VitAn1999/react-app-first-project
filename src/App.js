8900afe5c517fb4cb609ae5b1d2f30c6c0633c2e
Виталий Рощупкин <vroshchupkin@Magnum.local>
2021-02-11 17:13:17 +0300

    create Footer.jsx

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Navbar from './components/Navbar/Navbar';
 import Messages from './components/Messages/Messages';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className="wrapper">
       <Header />
       <Navbar />
-      <Messages />
+      <Profile />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import classes from './Footer.module.css';
+
+const Footer = (props) => {
+  return (
+    <footer className={classes.footer}>
+      <a className={classes['footer__link']}>Lorem</a>
+      <a className={classes['footer__link']}>Lorem</a>
+      <a className={classes['footer__link']}>Lorem</a>
+      <a className={classes['footer__link']}>Lorem</a>
+    </footer>
+  );
+};
+
+export default Footer;
