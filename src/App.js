a788f3705fb87b846b2e61c2c968ff4dad8dac6f
Виталий Рощупкин <vroshchupkin@Magnum.local>
2021-02-22 17:02:14 +0300

    create state and transfer store from index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Profile from './components/Profile/Profile';
-import Navbar from './components/Navbar/Navbar';
-import Messages from './components/Messages/Messages';
-import Music from './components/Music/Music';
-import News from './components/News/News';
-import Settings from './components/Settings/Settings';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
+import Messages from "./components/Messages/Messages";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+import Footer from "./components/Footer/Footer";
 
 const App = (props) => {
   return (
---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+const state = {
+  postData: [
+    { id: 1, post: "какулька", count: "5" },
+    { id: 2, post: "Бэтмен", count: "12" },
+    { id: 3, post: "какулька", count: "8" },
+    { id: 4, post: "Николас Кейдж", count: "15" },
+  ],
+  usersData: [
+    {
+      id: 1,
+      userName: "Anton",
+      src:
+        "https://pm1.narvii.com/6825/d74beddc6e5570160f786213f68d740dc0c30834v2_00.jpg",
+    },
+    {
+      id: 2,
+      userName: "Viktor",
+      src:
+        "https://cdn.freelance.ru/img/portfolio/pics/00/36/88/3573970.jpg?mt=c7bcef8b",
+    },
+    {
+      id: 3,
+      userName: "Ales'",
+      src: "https://cdn1.flamp.ru/6e8b1e5fe1cc01a4d15e1d27c602dfa7.jpg",
+    },
+    {
+      id: 4,
+      userName: "Joshua",
+      src:
+        "https://i.pinimg.com/736x/2d/0e/41/2d0e419c310033945063c6c9884b2725.jpg",
+    },
+  ],
+};
